256a4714ed3792f54aa5c35ee03cb2b71b3fd27f
Remy Da Costa Faro <remy@dacostafaro.com>
2018-03-31 15:10:06 +0200

    Added API part

---
+++ app/api/users.py
@@ -0,0 +1,31 @@
+from app import db
+from app.api import api
+from flask import jsonify, url_for, request
+
+from app.api.errors import bad_request
+from app.models import User
+
+from app.api.auth import token_auth
+
+@api.route('/users/<int:id>', methods=['GET'])
+@token_auth.login_required
+def get_user(id):
+    return jsonify(User.query.get_or_404(id).to_dict())
+
+@api.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json() or {}
+    if 'username' not in data or 'email' not in data or 'password' not in data:
+        return bad_request('must include username, email and password fields')
+    if User.query.filter_by(username=data['username']).first():
+        return bad_request('please use a different username')
+    if User.query.filter_by(email=data['email']).first():
+        return bad_request('please use a different email address')
+    user = User()
+    user.from_dict(data, new_user=True)
+    db.session.add(user)
+    db.session.commit()
+    response = jsonify(user.to_dict())
+    response.status_code = 201
+    response.headers['Location'] = url_for('api.get_user', id=user.id)
+    return response
